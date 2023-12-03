29467495c8be4ba28ebc4883241c2aa313462e6a
Guilherme Puida <guilherme@puida.xyz>
2023-12-03 01:21:11 -0300

    Solve 2015 2

---
+++ aoc/solutions/y2015d02.py
@@ -0,0 +1,29 @@
+from aoc.lib.base_solution import BaseSolution
+
+
+class Solution(BaseSolution):
+    def setup(self):
+        self.dimensions = [tuple(map(int, line.split("x"))) for line in self.lines]
+
+    def part_one(self):
+        total = 0
+
+        for l, w, h in self.dimensions:
+            lw = l * w
+            wh = w * h
+            hl = h * l
+
+            total += 2 * lw + 2 * wh + 2 * hl + min(lw, wh, hl)
+
+        return total
+
+    def part_two(self):
+        total = 0
+
+        for l, w, h in self.dimensions:
+            volume = l * w * h
+            s1, s2, _ = sorted([l, w, h])
+
+            total += 2 * s1 + 2 * s2 + volume
+
+        return total
