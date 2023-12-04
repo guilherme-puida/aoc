5aad578c239fd13ec23ac6ceb997d31d364f8d74
Guilherme Puida <guilherme@puida.xyz>
2023-12-04 08:29:49 -0300

    Solve 2017 1

---
+++ aoc/solutions/y2017d01.py
@@ -0,0 +1,25 @@
+from aoc.lib.base_solution import BaseSolution
+
+
+class Solution(BaseSolution):
+    def setup(self):
+        self.input_len = len(self.input_str)
+        pass
+
+    def part_one(self):
+        total = 0
+
+        for i, c in enumerate(self.input_str):
+            if c == self.input_str[(i + 1) % self.input_len]:
+                total += int(c)
+
+        return total
+
+    def part_two(self):
+        total = 0
+
+        for i, c in enumerate(self.input_str):
+            if c == self.input_str[(i + (self.input_len // 2)) % self.input_len]:
+                total += int(c)
+
+        return total
