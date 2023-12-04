a5d406619433c3419c96c6934c622a4d3786253f
Guilherme Puida <guilherme@puida.xyz>
2023-12-03 23:22:20 -0300

    Solve 2015 7

---
+++ aoc/solutions/y2015d07.py
@@ -0,0 +1,45 @@
+from aoc.lib.base_solution import BaseSolution
+
+import functools
+
+
+class Solution(BaseSolution):
+    def setup(self):
+        self.data = {}
+
+        for line in self.lines:
+            command, dest = line.split(" -> ")
+            self.data[dest] = command
+
+    def part_one(self):
+        self.result = str(self.evaluate("a"))
+        return self.result
+
+    def part_two(self):
+        if not self.result:
+            self.part_one()
+
+        self.data["b"] = self.result
+        self.evaluate.cache_clear()
+        return self.evaluate("a")
+
+    @functools.lru_cache
+    def evaluate(self, key):
+        try:
+            return int(key)
+        except ValueError:
+            pass
+
+        match self.data[key].split():
+            case [x]:
+                return self.evaluate(x)
+            case ["NOT", x]:
+                return ~self.evaluate(x)
+            case [x, "AND", y]:
+                return self.evaluate(x) & self.evaluate(y)
+            case [x, "OR", y]:
+                return self.evaluate(x) | self.evaluate(y)
+            case [x, "LSHIFT", y]:
+                return self.evaluate(x) << self.evaluate(y)
+            case [x, "RSHIFT", y]:
+                return self.evaluate(x) >> self.evaluate(y)
